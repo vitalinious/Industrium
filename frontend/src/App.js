5955fd6bdc3b1b0bf129b7f716f63171ad9a84d2
Vitalik <vitalik@example.com>
2025-05-01 20:44:29 +0300

    Fixed white screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login     from './Login';
 import Register  from './Register';
